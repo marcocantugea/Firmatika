e530121514a93b373b02ef90576ed7a4019524a7
Marco A. Cantu Gea <marco.cantu.g@gmail.com>
2025-11-17 21:12:25 -0600

    Update UserBasicInfo model to make wallet_address optional

--- src/firmatika-api/models/userInfo.py
+++ src/firmatika-api/models/userInfo.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class UserBasicInfo(BaseModel):
     uuid: str
     nombres: str
     apellidos: str
     email: str
-    wallet_address: str
+    wallet_address: Optional[str] = None
